221594014daa0f02acc05e070c4527572c4c41e1
Cutina Woof <gretchenista@gmail.com>
2018-01-11 16:01:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function happyHolidayTo(holiday, name) {
   return "Happy ${holiday}, ${name}!"
 }
 
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holiday, days) {
   return "It's ${days} days until ${holiday}!"
 }
 
 happyHolidaysTo("you");
 happyHolidayTo("Independence Day", "you");
-holidayCountdown("20", "Mother's Day");
+holidayCountdown("Mother's Day", 20);
